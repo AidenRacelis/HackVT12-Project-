5d6f5dad9b8c5672b464a2fc9fb42f9256ea2b42
David O'Regan <84041288+dave-oregan@users.noreply.github.com>
2024-09-14 15:06:51 -0400

    Update App.js
    - fix merge conflict

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Chatbot from './Chatbot'; 
 import Profile from './Profile';
-
+import Settings from './Settings.s';
 
 function App() {
   return (
     <div className="App">
       <Chatbot />
       <Profile />
+      <Settings />
+      <About />
     </div>
   );
 }
