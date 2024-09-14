ec6047a42720ae6d51252f52099436aba0671848
David O'Regan <oreagn10@gmail.com>
2024-09-14 18:36:31 -0400

    Procedural Generation of Houses

--- src/components/SearchHouses.js
+++ src/components/SearchHouses.js
@@ -28,19 +28,56 @@ export default function SearchHouses() {
         </span>
       </div>
       <div className='DisplayHouses'>
-        <div className='HouseDiv' value={`${house_list[0].home_type}`}>
-          <img className='HouseImg' src='../assets/House-Modern.png'></img>
-          <span className='price' value={`${house_list[0].price}`}>${house_list[0].price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</span>
-          <span className='list_item'>{house_list[0].size} Sq Ft</span>
-          <span className='list_item'></span>
-          <span className='list_item'>{formatString(house_list[0].setting)}</span>
+        {/* <div className='HouseDiv' value={`${house_list[0].home_type}`}> */}
+          {/* <img className='HouseImg' src='../assets/House-Modern.png'></img> */}
+          {/* <span className='price' value={`${house_list[0].price}`}>${house_list[0].price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</span> */}
+          {/* <span className='list_item'>{house_list[0].size} Sq Ft</span> */}
+          {/* <span className='list_item'>{house_list[0].bedrooms} bed, {house_list[0].bathrooms} bath</span> */}
+          {/* <span className='list_item'>{house_list[0].parking_am} Car Parking</span> or if house_list[0].parking_garage is true then <span className='list_item'>{house_list[0].parking_am} Car Garage</span> */}
+          {/* <span className='list_item'>{formatString(house_list[0].setting)}</span> */}
+          {/*if house_list[0].schools (great, good, mid, poor, bad) is true then <span className='list_item'>star emoji for each, 1 for bad, 5 for good</span>*/}
+          {/*if house_list[0].gated is true then <span className='list_item'>{house_list[0].gated}</span>*/}
+          {/*if house_list[0].pool is true then <span className='list_item'>{house_list[0].pool}</span>*/}
+          {/*if house_list[0].AC and/or house_list[0].heat are true then <span className='list_item'>A/C, Heat</span>*/}
+          {/*if house_list[0].pool is true then <span className='list_item'>{house_list[0].pool}</span>*/}
+        {/* </div> */}
+        {house_list.map((house, index) => (
+        <div key={index} className='HouseDiv' value={`${house.home_type}`}>
+          <img className='HouseImg' src='../assets/House-Modern.png' alt='House'></img>
+          
+          <span className='price' value={`${house.price}`}>
+            ${house.price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+          </span>
+          
+          <span className='list_item'>{house.size} Sq Ft</span>
+          <span className='list_item'>{house.bedrooms} bed, {house.bathrooms} bath</span>
+          
+          {house.parking_garage ? (
+            <span className='list_item'>{house.parking_am} Car Garage</span>
+          ) : (
+            <span className='list_item'>{house.parking_am} Car Parking</span>
+          )}
+
+          <span className='list_item'>{formatString(house.setting)}</span>
+
+          {/* Display stars for school rating */}
+          {house.schools && (
+            <span className='list_item'>
+              {"★".repeat(house.schools)} {/* Repeat stars based on school rating */}
+            </span>
+          )}
+
+          {house.gated && <span className='list_item'>Gated Community</span>}
+
+          {house.pool && <span className='list_item'>Pool</span>}
+
+          {(house.AC || house.heat) && (
+            <span className='list_item'>
+              {house.AC ? 'A/C' : ''}{house.AC && house.heat ? ', ' : ''}{house.heat ? 'Heat' : ''}
+            </span>
+          )}
         </div>
-        <div className='HouseDiv' value={`${house_list[0].home_type}`}>
-          <img className='HouseImg' src='../assets/House-Modern.png'></img>
-          <span className='price' value={`${house_list[0].price}`}>${house_list[0].price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</span>
-          <span className='list_item'>{house_list[0].size} Sq Ft</span>
-          <span className='list_item'>{formatString(house_list[0].setting)}</span>
-        </div>
+      ))}
       </div>
     </div>
   )
