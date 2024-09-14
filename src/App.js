cf27cd8d6e808eeb6b9dcafda451576dcb5c0b29
Richard Raad <richardtraad@icloud.com>
2024-09-14 14:02:46 -0400

    Reorganized all files
    Moved all files out of my-app.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Chatbot from './Chatbot'; 
+import Profile from './Profile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
