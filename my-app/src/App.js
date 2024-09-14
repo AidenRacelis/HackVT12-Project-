006682f4705e7a62246a88eee9db08a433711b8d
David Luu <davidluu@MacBook-Pro.local>
2024-09-14 09:49:28 -0400

    Added UI #1

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Chatbot from './Chatbot'; 
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
