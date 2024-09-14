4f40b77990839923e543982da2ecf3acd13de173
AidenRacelis <158542367+AidenRacelis@users.noreply.github.com>
2024-09-14 13:48:33 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Chatbot from './Chatbot'; 
-import Review from './Review';
+import Review from './Profile';
 
 
 function App() {
   return (
     <div className="App">
       <Chatbot />
-      <Review />
+      <Profile />
     </div>
   );
 }
